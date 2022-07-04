a33ee05485cf87026423e07b9a9e5753c8b554f4
hyewonPark <123pho@naver.com>
2022-07-05 00:10:43 +0900

    feat: 초기설정

---
+++ src/main/java/com/triple/triplep/TriplePApplication.java
@@ -0,0 +1,13 @@
+package com.triple.triplep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriplePApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriplePApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/triple/triplep/TriplePApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.triplep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplePApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
